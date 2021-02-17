9b1b2d8e980d7cd2023a7f487a463f041866fb0e
AndreaG <gaggiandrea@gmail.com>
2021-02-17 14:53:34 +0100

    PageController for routing

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+            }
+
+            header {
+                background-color: slategray;
+                color: white;
+                padding: 2rem 0;
+            }
+
+            .centered {
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <header class="centered">
+            <h1>Blog</h1>
+        </header>
+        <section>
+            <h2>posts</h2>
+        </section>
+    </body>
+</html>
